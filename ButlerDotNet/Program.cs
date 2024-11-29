a84165c6e30ee0c7075d7a11f67f680204562080
Ellpeck <me@ellpeck.de>
2024-11-29 16:23:49 +0100

    added proper exception handling

--- ButlerDotNet/Program.cs
+++ ButlerDotNet/Program.cs
@@ -18,38 +18,44 @@ namespace ButlerDotNet {
             Console.WriteLine($"Channel: {channel}");
 
             // check which butler version we have
-            Version version;
+            var version = new Version();
             var versionFile = new FileInfo(Path.Combine(dll.Directory.FullName, "ButlerVersion.txt"));
-            if (versionFile.Exists) {
-                using var reader = versionFile.OpenText();
-                if (!Version.TryParse(await reader.ReadToEndAsync(), out version))
-                    version = new Version();
-            } else {
-                version = new Version();
+            try {
+                if (versionFile.Exists) {
+                    using var reader = versionFile.OpenText();
+                    if (Version.TryParse(await reader.ReadToEndAsync(), out var parsed))
+                        version = parsed;
+                }
+                Console.WriteLine($"Installed version: {version}");
+            } catch (Exception e) {
+                Console.WriteLine("Failed to read installed version:\n" + e);
             }
-            Console.WriteLine($"Installed version: {version}");
 
             // get latest version of butler
-            using var client = new HttpClient();
-            var link = $"https://broth.itch.zone/butler/{channel}/LATEST";
-            var latest = await client.GetStringAsync(link);
-            Console.WriteLine($"Latest version: {latest}");
+            try {
+                using var client = new HttpClient();
+                var link = $"https://broth.itch.zone/butler/{channel}/LATEST";
+                var latest = await client.GetStringAsync(link);
+                Console.WriteLine($"Latest version: {latest}");
 
-            // update butler if necessary
-            if (!Version.TryParse(latest, out var latestVersion) || latestVersion > version) {
-                Console.WriteLine("Updating butler to " + latestVersion);
-                var zipFile = Path.Combine(dll.Directory.FullName, "butler.zip");
-                await using (var httpStream = await client.GetStreamAsync($"{link}/archive/default")) {
-                    await using (var fileStream = File.Create(zipFile))
-                        await httpStream.CopyToAsync(fileStream);
+                // update butler if necessary
+                if (!Version.TryParse(latest, out var latestVersion) || latestVersion > version) {
+                    Console.WriteLine("Updating butler to " + latestVersion);
+                    var zipFile = Path.Combine(dll.Directory.FullName, "butler.zip");
+                    await using (var httpStream = await client.GetStreamAsync($"{link}/archive/default")) {
+                        await using (var fileStream = File.Create(zipFile))
+                            await httpStream.CopyToAsync(fileStream);
+                    }
+                    ZipFile.ExtractToDirectory(zipFile, butlerDir.FullName, true);
+                    File.Delete(zipFile);
+                    Console.WriteLine("Finished updating butler to " + latestVersion);
+
+                    // update latest version file
+                    await using var writer = versionFile.CreateText();
+                    writer.Write(latestVersion);
                 }
-                ZipFile.ExtractToDirectory(zipFile, butlerDir.FullName, true);
-                File.Delete(zipFile);
-                Console.WriteLine("Finished updating butler to " + latestVersion);
-
-                // update latest version file
-                await using var writer = versionFile.CreateText();
-                writer.Write(latestVersion);
+            } catch (Exception e) {
+                Console.WriteLine("Failed to update butler:\n" + e);
             }
 
             // run butler with our arguments
